65a98a0bfa537982af45c3cb8a2dd4370f373db8
shahcaf <viki222222997@gmail.com>
2025-11-05 12:34:32 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Add any future JavaScript functionality here
+console.log('Splashy bot website is ready!');
+
+// Example: Add smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
